24320a887de924ff65ade1ce0149396420f3dfb8
Adanion12 <andrija.markovic.mne@gmail.com>
2018-06-27 20:15:32 -0500

    Windows integration (#1)
    Remove extra scripts section and add Windows integration

---
+++ src/pages/Feedback.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <v-layout align-center>
+            <v-form>
+                <h2 class="title">Send Feedback</h2>
+                <v-snackbar/>
+                <v-text-field
+                    label="Subject"
+                    id="subject"/>
+            </v-form>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+   export default{
+       name: 'Feedback',
+   };  
+</script>
+
+<style>
+
+</style>
